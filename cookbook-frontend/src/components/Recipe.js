0d01ab48838b05a796b10b655b58be422ef2e5a9
Exeiya <krista.lindholm@gmail.com>
2019-05-15 19:38:29 +0300

    Add styles to recipe view

--- cookbook-frontend/src/components/Recipe.js
+++ cookbook-frontend/src/components/Recipe.js
@@ -1,30 +1,42 @@
 import React from 'react';
-import { Table } from 'semantic-ui-react';
+import { Table, Grid, Image } from 'semantic-ui-react';
+import picture from '../assets/default_picture.jpg';
 
 const Recipe = ({ recipe }) => {
   if (!recipe) return null;
 
   return (
-    <div style={{ border: 'solid 1px', padding: 5, margin: 5 }}>
-      <h3>{recipe.title}</h3>
-      <Table collapsing>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell colSpan="2">Ainekset</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {recipe.ingredients.map(ingredient =>
-            <Table.Row key={ingredient.name} >
-              <Table.Cell>{ingredient.amount ? ingredient.amount : null}</Table.Cell>
-              <Table.Cell>{ingredient.name}</Table.Cell>
-            </Table.Row>
-          )}
-        </Table.Body>
-      </Table>
-      <h4>Ohjeet</h4>
-      <p>{recipe.instructions}</p>
-    </div>
+    <Grid centered stackable>
+      <Grid.Row>
+        <Grid.Column width={12}>
+          <h3>{recipe.title}</h3>
+          <Image src={picture} size="medium" rounded/>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column width={4}>
+          <Table unstackable>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell colSpan="2">Ainekset</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {recipe.ingredients.map(ingredient =>
+                <Table.Row key={ingredient.name} >
+                  <Table.Cell>{ingredient.amount ? ingredient.amount : null}</Table.Cell>
+                  <Table.Cell>{ingredient.name}</Table.Cell>
+                </Table.Row>
+              )}
+            </Table.Body>
+          </Table>
+        </Grid.Column>
+        <Grid.Column width={8}>
+          <h4>Ohjeet</h4>
+          <p>{recipe.instructions}</p>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   );
 };
 
