514efc3a1a53fd94c4a9dd541b4208f130b8baef
Exeiya <krista.lindholm@gmail.com>
2019-06-05 23:17:43 +0300

    Add a simple search to recipe view

--- cookbook-frontend/src/components/RecipeList.js
+++ cookbook-frontend/src/components/RecipeList.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { Card, Grid, } from 'semantic-ui-react';
+import { Card, Grid, Input } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import picture from '../assets/default_picture.jpg';
 
 const RecipeList = ({ recipes }) => {
+  const [ filter, setfilter ] = useState('');
 
-  const recipeList = recipes.map(recipe =>
+  const recipesToShow = filter
+    ? recipes.filter(recipe => recipe.title.toLowerCase().includes(filter.toLowerCase()))
+    : recipes;
+
+  const recipeList = recipesToShow.map(recipe =>
     <Grid.Column key={recipe.id}>
       <Card as={Link} to={`/recipes/${recipe.id}`} color="teal" image={picture} header={recipe.title}/>
     </Grid.Column>
   );
 
   return (
-    <Grid centered columns={3}>
-      <Grid.Column />
-      <Grid.Column width={14}>
-        <Grid stackable doubling columns={3}>{recipeList}</Grid>
-      </Grid.Column>
-      <Grid.Column />
+    <Grid >
+      <Grid.Row centered columns={1}>
+        <Grid.Column textAlign="center" width={4}>
+          <h3>Hae reseptejä</h3>
+          <Input icon="search" onChange={({ target }) => setfilter(target.value)} value={filter} />
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row centered columns={3}>
+        <Grid.Column />
+        <Grid.Column width={14}>
+          <Grid stackable doubling columns={3}>{recipeList}</Grid>
+        </Grid.Column>
+        <Grid.Column />
+      </Grid.Row>
     </Grid>
   );
 };
