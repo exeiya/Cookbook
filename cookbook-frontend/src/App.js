20facf3d2ed2c1a4a3eb1212b0175fde791cf339
Exeiya <krista.lindholm@gmail.com>
2019-06-05 23:16:58 +0300

    Modify recipe view routing & add dashboard component as front page

--- cookbook-frontend/src/App.js
+++ cookbook-frontend/src/App.js
@@ -6,6 +6,7 @@ import RecipeForm from './components/RecipeForm';
 import RecipeList from './components/RecipeList';
 import Recipe from './components/Recipe';
 import Notification from './components/Notification';
+import Dashboard from './components/Dashboard';
 import { initializeRecipes } from './reducers/recipeReducer';
 
 function App(props) {
@@ -25,10 +26,12 @@ function App(props) {
         <h1>Cookbook</h1>
         <Menu>
           <Menu.Item as={Link} to="/" name="Etusivu" />
+          <Menu.Item as={Link} to="/recipes" name="Reseptit" />
           <Menu.Item as={Link} to="/createNewRecipe" name="Uusi resepti" />
         </Menu>
         <Notification />
-        <Route exact path="/" render={() => <RecipeList />} />
+        <Route exact path="/" render={() => <Dashboard />} />
+        <Route exact path="/recipes" render={() => <RecipeList />} />
         <Route path="/createNewRecipe" render={() => <RecipeForm />} />
         <Route exact path="/recipes/:id" render={({ match }) => <Recipe recipe={recipeById(match.params.id)}/>} />
       </Router>
---
+++ cookbook-frontend/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h2>Tervetuloa!</h2>
+      <p></p>
+    </div>
+  );
+};
+
+export default Dashboard;
