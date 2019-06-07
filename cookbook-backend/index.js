64fa5f3da927ca8b08be05819f8188623ee481ef
Exeiya <krista.lindholm@gmail.com>
2019-06-07 19:58:10 +0300

    Implement adding a new recipe

--- cookbook-backend/index.js
+++ cookbook-backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-const recipes = [
+app.use(bodyParser.json());
+
+let recipes = [
   {
     id: 1,
     title: 'Mustikkapiirakka',
@@ -42,6 +45,70 @@ const recipes = [
 
 app.get('/api/recipes', (req, res) => {
   res.json(recipes);
+});
+
+app.get('/api/recipes/:id', (req, res) => {
+  const recipe = recipes
+    .find(recipe => recipe.id === Number(req.params.id));
+  
+  if (recipe) res.json(recipe);
+  res.status(404).end();
+});
+
+const validateFields = (recipe) => {
+  const { 
+    title, 
+    instructions,
+    category,
+    ingredients
+  } = recipe;
+
+  if (!title || !instructions || !category || !ingredients) {
+    return ({ error: 'Missing required fields' });
+  } else if (!Array.isArray(ingredients) || ingredients.length === 0) {
+    return ({ error: 'Missing ingredients'})
+  }
+
+  let ingredientsError = null;
+  ingredients.forEach(ingredient => {
+    if (!ingredient.name) {
+      ingredientsError = ({ error: 'Missing ingredient name'});
+      return;
+    }
+  });
+
+  return ingredientsError;
+};
+
+app.post('/api/recipes', (req, res) => {
+  const { 
+    title, 
+    instructions,
+    category,
+    ingredients,
+    servings,
+    cookingTime
+  } = req.body;
+
+  const validationError = validateFields(req.body);
+
+  if (!validationError) {
+    const newRecipe = {
+      id: recipes.length + 1,
+      title,
+      instructions,
+      category,
+      ingredients: ingredients.map(ingredient =>
+        ({ name: ingredient.name, amount: ingredient.amount || '' })
+      ),
+      servings: servings || null,
+      cookingTime: cookingTime || null
+    }
+    recipes.push(newRecipe);
+    return res.status(201).send(newRecipe);
+  }
+  
+  res.status(400).send(validationError);
 });
 
 const PORT = 3001;
