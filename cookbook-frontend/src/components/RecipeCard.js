3201a09dd71ffc046f4eaf12957e1e57632050fe
Exeiya <krista.lindholm@gmail.com>
2019-07-17 22:32:16 +0300

    Refactor recipe card to its own component

---
+++ cookbook-frontend/src/components/RecipeCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Grid, Card, Image } from 'semantic-ui-react';
+import picture from '../assets/default_picture.jpg';
+
+const RecipeCard = (recipe) => {
+  if (!recipe) return null;
+  return (
+    <Grid.Column key={recipe.id}>
+      <Card as={Link} to={`/recipes/${recipe.id}`} color="teal" style={{ minHeight: '210px' }}>
+        <Image src={recipe.imgUrl || picture} style={{ maxHeight: '170px' }}/>
+        <Card.Content>
+          <Card.Header style={{ overflow: 'hidden' }}>{recipe.title}</Card.Header>
+        </Card.Content>
+      </Card>
+    </Grid.Column>
+  );
+};
+
+export default RecipeCard;
