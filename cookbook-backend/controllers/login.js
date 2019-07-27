2a926ef91f186f2fa4942ef1a7a281d832d1959b
Exeiya <krista.lindholm@gmail.com>
2019-07-27 21:54:08 +0300

    Add error handling for login

--- cookbook-backend/controllers/login.js
+++ cookbook-backend/controllers/login.js
@@ -3,24 +3,28 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
-loginRouter.post('/', async (req, res) => {
+loginRouter.post('/', async (req, res, next) => {
   const { username, password } = req.body;
 
-  const user = await User.findOne({ username });
-  const authenticationSuccess = user === null
-    ? false
-    : await bcrypt.compare(password, user.passwordHash);
+  try {
+    const user = await User.findOne({ username });
+    const authenticationSuccess = user === null
+      ? false
+      : await bcrypt.compare(password, user.passwordHash);
 
-  if (!authenticationSuccess) {
-    return res.status(401).json({ error: 'Invalid username or password' });
+    if (!authenticationSuccess) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    const token = jwt.sign(
+      { username: user.username, id: user._id },
+      process.env.SECRET
+    );
+
+    return res.status(200).json({ token, username: user.username, id: user._id });
+  } catch (error) {
+    next(error);
   }
-
-  const token = jwt.sign(
-    { username: user.username, id: user._id },
-    process.env.SECRET
-  );
-
-  return res.status(200).json({ token, username: user.username, id: user._id });
 });
 
 module.exports = loginRouter;
